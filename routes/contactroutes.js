4de4c5c236ac9a42be15b69cefc9d653f18b1dd6
nicholas <aigberuan6@gmail.com>
2024-03-28 12:12:21 +0100

    routes

---
+++ routes/contactroutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+router.route('/').get( (req, res) =>{
+    //res.send("get all contacts");
+    res.status(200).json({
+        message: 'get all contacts',
+    })
+})
+
+
+router.route('/').post( (req, res) =>{
+    res.status(200).json({
+        message: 'create a new contact'
+    })
+})
+
+
+router.route('/:id').put( (req, res) =>{
+    //res.send("get all contacts");
+    res.status(200).json({
+        message: 'Update contacts'
+    })
+})
+
+
+router.route('/').get( (req, res) =>{
+    //res.send("get all contacts");
+    res.status(200).json({
+        message: 'Hello World!'
+    })
+})
+
+module.exports = router;
